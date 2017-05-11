b8cdd700f374c44d121e2cfaab2b63ad75062b20
Stormy Ramsey <stormy.ramsey@gmail.com>
2017-05-11 15:11:19 -0400

    added app.js

---
+++ goldie_nauts_angular/js/app.js
@@ -0,0 +1,45 @@
+"use strict";
+
+(function(){
+  angular
+  .module("goldie_nauts",[
+    "ui.router",
+    "ngResource"
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ])
+function RouterFunction($stateProvider){
+  $stateProvider
+  .state("astroIndex",{
+    url: "/astro",
+    templateUrl:"js/astro/index.html",
+    controller: "AstroIndexController",
+    controllerAs: "vm"
+  })
+  .state("astroShow",{
+    url: "astros/:id",
+    templateUrl: "js/astro/show.html",
+    controller: "AstrosShowController",
+    controllerAs: "vm"
+    })
+  }
+
+function RouterFunction($stateProvider){
+  $stateProvider
+  .state("planetIndex",{
+    url: "/planet/:id",
+    templateUrl: "js/planets/index.html",
+    controller: "PlanetIndexController",
+    controllerAs: "vm"
+  })
+  .state("planetShow",{
+  url: "/planet/:id",
+  templateUrl: "js/planets/index.html",
+  controller: "PlanetShowController",
+  controllerAs: "vm"
+  })
+}
+
+})();
