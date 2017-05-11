ae4fd005c993cd29494669c615dfee85a7f4c314
Adam Hoff <swimmerdude79@comcast.net>
2017-05-11 15:35:37 -0400

    created astros controller

--- goldie_nauts_api/app/controllers/astros_controller.rb
+++ goldie_nauts_api/app/controllers/astros_controller.rb
@@ -1,2 +1,14 @@
 class AstrosController < ApplicationController
+  def index
+    @astros = Astro.all
+    respond_to do |format|
+      format.json {render json: @astros}
+    end
+  end
+  def show
+    @astro = Astro.find(params[:id])
+    respond_to do |format|
+      format.json {render json: @astros}
+    end
+  end
 end
