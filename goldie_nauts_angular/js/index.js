0447a216b6e95c47356b4ffe95a4e223f79010e0
Jeong Hoon Koo <junghoone17@gmail.com>
2017-05-12 16:21:20 -0400

    working on index page

---
+++ goldie_nauts_angular/js/index.js
@@ -0,0 +1,57 @@
+
+
+
+
+$(function () {
+    $("#astroHiddenIndex").removeAttr("style");
+    $("#astroHiddenIndex").attr("width", "1px");
+    $("#astroMainIndex").fadeIn("fast");
+    $("#planetMainIndex").fadeIn("fast");
+
+    $("#astroWindowIndex").hide()
+    $("#planetMainIndex").hover(function () {
+        $(this).attr("src", "/background/earth.gif") ;
+
+    });
+    $("#planetMainIndex").mouseleave(function () {
+        $(this).attr("src", "/background/earth.png") ;
+
+    });
+
+    $("#astroMainIndex").click(function(){
+      $("#astroMainIndex").fadeOut("fast");
+      $("#planetMainIndex").fadeOut("fast");
+      $("#astroWindowIndex").fadeOut("fast");
+      let showHiddenFunc = function() {
+        $("#astroHiddenIndex").delay(600).fadeIn("slow");
+      }
+      let showHiddenAfterFunc = function() {
+        $("#astroHiddenIndex").css({
+          position: "fixed",
+          transform: "rotate(400deg)",
+          overflow: "hidden",
+          "width": "150%",
+          "max-width": "1000px",
+          right: "50px",
+          top: "200px",
+          transition: "all 4.6s linear"
+        });
+      }
+      $.when(showHiddenFunc()).done(showHiddenAfterFunc());
+      setTimeout(function() {
+        $("#astroWindowIndex").show()
+      }, 4400);
+      setTimeout(function() {
+        $("#astroHiddenIndex").removeAttr("style");
+        $("#astroHiddenIndex").attr("width", "1px");
+        $("#astroMainIndex").fadeIn("fast");
+        $("#planetMainIndex").fadeIn("fast");
+        window.location.href = "http://localhost:8080/#/astro";
+      }, 6000);
+
+
+    })
+
+
+
+});
