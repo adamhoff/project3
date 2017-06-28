e049aa281252e9da14773bf62012983a406ac2d0
Jeong Hoon Koo <junghoone17@gmail.com>
2017-06-27 23:46:51 -0400

    test

--- goldie_nauts_api/app/models/astro.rb
+++
@@ -1,2 +0,0 @@
-class Astro < ApplicationRecord
-end
--- goldie_nauts_api/test/controllers/astros_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class AstrosControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
